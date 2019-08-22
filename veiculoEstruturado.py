c02502f8d589c0738e5b3b26a701198b4bc7caff
Anderson dos Santos <andersoon_12@hotmail.com>
2019-08-21 22:58:34 -0300

    entrega-[1]
    asdas

---
+++ veiculoEstruturado.py
@@ -0,0 +1,5 @@
+user = int(input('Digite o numero para o tipo de carro:\n1 - Carro\n2 - Moto\n'))
+if user == 1:
+    print('O carro possui: 4 rodas')
+elif user == 2:
+    print('A moto possui: 2 rodas')
